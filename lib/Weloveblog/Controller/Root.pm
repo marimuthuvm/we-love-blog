14ba3d548597e17970bb3a87dd1bc176f29c60fd
Nitipit Nontasuwan <um@abctech-thailand.com>
2011-03-25 10:55:59 +0700

    - SAND-32 : Edit wrapper.tt and Add Home Template

--- lib/Weloveblog/Controller/Root.pm
+++ lib/Weloveblog/Controller/Root.pm
@@ -28,9 +28,9 @@ The root page (/)
 
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
-
-    # Hello World
-    $c->response->body( $c->welcome_message );
+    $c->stash(
+        template => 'template/home.tt',
+    );
 }
 
 =head2 default
