087e536f6b7a63860b59c6aee493c6b45a81a7f8
Ronnachate Gatekieo <yo@abctech-thailand.com>
2011-03-25 12:10:09 +0700

    SAND-45 create dashboard page fot temporary landing page

---
+++ lib/Weloveblog/Controller/Dashboard.pm
@@ -0,0 +1,54 @@
+package Weloveblog::Controller::Dashboard;
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+Weloveblog::Controller::Dashboard - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    if (!$c->user_exists) {
+            # Dump a log message to the development server debug output
+            $c->log->debug('***User not found, forwarding to /login');
+            # Redirect the user to the login page
+            $c->response->redirect($c->uri_for('/login'));
+            # Return 0 to cancel 'post-auto' processing and prevent use of application
+            return 0;
+    }
+    else {
+    	 $c->stash(template => 'template/dashboard/index.tt');
+    }
+
+}
+
+
+=head1 AUTHOR
+
+Yo!!!,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
---
+++ t/controller_Dashboard.t
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+use Test::More;
+
+use ok 'Test::WWW::Mechanize::Catalyst' => 'Weloveblog';
+
+ok( my $mech = Test::WWW::Mechanize::Catalyst->new, 'Created mech object' );
+
+#Call logout first to clear any session.
+$mech->get_ok( 'http://localhost:3000/logout' );
+#Get to dash board without any authentication
+$mech->get_ok( 'http://localhost:3000/dashboard' );
+$mech->content_contains('Password');
+
+#Get to dash board from login page
+$mech->get_ok( 'http://localhost:3000/login');
+$mech->field('username', 'user');
+$mech->field('password', 'pass');
+$mech->submit_form_ok();
+$mech->content_contains('Dashboard for User');
+
+done_testing();
